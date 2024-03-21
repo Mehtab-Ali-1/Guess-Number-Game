097dc909b8fb7c29d56ac5ca1ccb4d1366226f3a
Mehtab-Ali-1 <160261202+Mehtab-Ali-1@users.noreply.github.com>
2024-03-21 06:14:14 -0700

    Add files via upload

---
+++ guessNumber.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const systemGeneratedNumber = Math.floor(Math.random() * 10);
+
+const answer = await inquirer.prompt([
+  {
+    type: "Number",
+    name: "guessNumber",
+    Message: "Guess a number between 1 to 10",
+  },
+]);
+
+if (answer.guessNumber === systemGeneratedNumber) {
+  console.log("Your guess is correct \n You win");
+} else {
+  console.log("Better luck try next time");
+}
+console.log(
+  "Your guess:",
+  answer.guessNumber,
+  "systemGeneratedNumber:",
+  systemGeneratedNumber
+);
